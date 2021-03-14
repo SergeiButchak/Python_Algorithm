b3d7dec66d6b5a026c7f7aee525dd32cb14d59d4
Sergei.Butchak <Sergei.Butchak@gmail.com>
2021-03-14 15:58:47 +0400

    task_2.py completed

--- Lesson_1/task_2.py
+++ Lesson_1/task_2.py
@@ -19,3 +19,38 @@
 Алгоритмизатор должен развивать мышление, а это прежде всего практика.
 А без столкновения со сложностями его не развить.
 """
+import random
+
+
+# O(n)
+def find_min(lst_obj: list):
+    if len(lst_obj) > 0:
+        min_el = lst_obj[0]
+        for ind in range(1, len(lst_obj)):
+            if min_el > lst_obj[ind]:
+                min_el = lst_obj[ind]
+        return min_el
+    else:
+        return None
+
+
+# O(n^2)
+def find_min_v2(lst_obj: list):
+    def is_min(list_obj: list, el: int):
+        result = True
+        for ind in range(1, len(list_obj)):
+            if el > list_obj[ind]:
+                result = False
+
+        return result
+
+    for ind in range(1, len(lst_obj)):
+        if is_min(lst_obj, lst_obj[ind]):
+            return lst_obj[ind]
+
+
+arr = list([random.randrange(1, 30) for el in range(1, 16)])
+
+print(arr)
+print(find_min(arr))
+print(find_min_v2(arr))
