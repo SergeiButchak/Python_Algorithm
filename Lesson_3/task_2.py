09862e83ad73eb403d6aea561aaac638fdfb8bca
Sergei.Butchak <Sergei.Butchak@gmail.com>
2021-03-23 18:45:30 +0400

    task_2.py completed

--- Lesson_3/task_2.py
+++ Lesson_3/task_2.py
@@ -18,3 +18,41 @@
 Допускаются любые усложения задания - валидация, подключение к БД, передача данных в файл
 """
 # sqlite, postgres, db_api, orm
+
+from hashlib import sha256
+
+
+def get_hash(password: str, salt: str):
+    return sha256(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+
+
+def save_pass(str_hash: str, file_name: str):
+    with open(file_name, 'a') as file_out:
+        file_out.write(str_hash)
+        file_out.write('\n')
+
+
+def check_password(password: str, salt: str, file_name: str):
+    res = False
+    str_hash = get_hash(password, salt)
+    with open(file_name) as file_in:
+        for line in file_in:
+            if line.strip() == str_hash:
+                res = True
+                break
+    return res
+
+
+file_nm = "pass.txt"
+str_salt = "SecretKey"
+
+# for i in range(0, 5):
+#     str_pass = input("Введите пароль: ")
+#     save_pass(get_hash(str_pass, str_salt), file_nm)
+
+str_pass = input("Введите пароль: ")
+chk_res = check_password(str_pass, str_salt, file_nm)
+if chk_res:
+    print("Пароль верен.")
+else:
+    print("Пароль неправильный.")
